43fc1e8f3548868dfb174c5ffc30032f4103cfd2
Bastian Widmer <b.widmer@dasrecht.net>
2013-04-22 14:59:24 +0200

    Added Checks for Service Host Requests

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
+//error_reporting(0);
+
 // include the library
 include 'dispatch.php';
 
 // load the configuration
 config('source', 'config/config.ini');
-
 // define your routes
+
+if (in_array($_SERVER['HTTP_HOST'], config('servicehost'))) {
+    echo "Service is up - You are connecting with a Servicehost";
+    die();
+}
+
 get('/index', function () {
     $domains = cache('domains', function() {
         _log(date(DATE_ISO8601).' - [INFO] - Reloading Data');
@@ -18,20 +25,20 @@ get('/index', function () {
         return $redirect;
     },config('cache.ttl'));
 
-    if($domains[$_SERVER['HTTP_HOST']] == true) {
+    if(array_key_exists($_SERVER['HTTP_HOST'], $domains)) {
         _log(date(DATE_ISO8601).' - [SUCCESS] - REDIRECTED '.$_SERVER['HTTP_HOST'].' >> '.trim($domains[$_SERVER['HTTP_HOST']]));
         redirect($domains[$_SERVER['HTTP_HOST']]);
-        //header('X-Powered-by : AWESOME REDIRECTR!');
     }
     else
     {
-        echo "No configuration found for this Domain!";
+        echo "No configuration found for this Domain! - " .$_SERVER['HTTP_HOST'] ;
         _log(date(DATE_ISO8601).' - [ERROR] - Domain '.$_SERVER['HTTP_HOST'].' has no config');
     }
 
 });
 get('/clearcache', function(){
     cache_invalidate('domains');
+    echo "Cache flushed";
     _log(date(DATE_ISO8601).' - [INFO] - Cache flushed');
 
 });
