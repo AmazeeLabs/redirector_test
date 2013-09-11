bd712bb7e147977d57c19d90819b7ac5994b5672
Bastian Widmer <b.widmer@dasrecht.net>
2013-09-11 15:34:44 +0200

    Rewrite to use composer and dispatch2.x

--- index.php
+++ index.php
@@ -1,56 +1,59 @@
 <?php
 //error_reporting(0);
-
 // DONT CACHE!
 header("Expires: Sun, 07 Aug 1987 12:34:56 GMT");
 header('Cache-Control: no-cache');
 header('Pragma: no-cache');
-
 // include the library
-include 'dispatch.php';
-
+include 'vendor/autoload.php';
 // load the configuration
 config('source', 'config/config.ini');
 // define your routes
-
 if (in_array($_SERVER['HTTP_HOST'], config('servicehost'))) {
     echo "Service is up - You are connecting with a Servicehost";
     die();
 }
 
-get('/index', function () {
+// Handle Paths. If we encounter a path we dynamically use this as our router if not we use the /
+if (isset($_SERVER['PATH_INFO']))
+{
+    $url = $_SERVER['PATH_INFO'];
+}
+else
+{
+    $url = '/';
+
+}
+
+on('*', $url, function () {
     $http_host = strtolower($_SERVER['HTTP_HOST']);
-    $domains = cache('domains', function() {
-        _log(date(DATE_ISO8601).' - [INFO] - Reloading Data');
-        $data = glob('redir/*');
+    $data = glob('redir/*');
 
-        foreach ($data as $d) {
-            // load redirections from filesystem
-            $redirect[str_replace('redir/', '',$d)] = file_get_contents($d);
-            // modify array key to match www. domains too
-            $redirect[str_replace('redir/', 'www.',$d)] = file_get_contents($d);
-        }
-
-        return $redirect;
-    },config('cache.ttl'));
-
-    if(array_key_exists($http_host, $domains)) {
-        _log(date(DATE_ISO8601).' - [SUCCESS] - REDIRECTED '.$http_host.' >> '.trim($domains[$http_host]));
-        redirect($domains[$http_host]);
+    foreach ($data as $d) {
+        // load redirections from filesystem
+        $redirect[strtolower(str_replace('redir/', '', $d)) ] = trim(file_get_contents($d));
+        // modify array key to match www. domains too
+        $redirect[strtolower(str_replace('redir/', 'www.', $d)) ] = trim(file_get_contents($d));
     }
-    else
-    {
-        echo "No configuration found for this Domain! - " .$http_host ;
-        _log(date(DATE_ISO8601).' - [ERROR] - Domain '.$http_host.' has no config');
+    $domains = $redirect;
+    if (array_key_exists($http_host, $domains)) {
+        send_remote_syslog('[SUCCESS] - REDIRECTED '.$http_host.' >> '.trim($domains[$http_host]));
+        redirect($domains[$http_host] . $_SERVER['REQUEST_URI'], 301);
+
+    } else {
+        echo "No configuration found for this Domain! - " . $http_host;
+        send_remote_syslog('[ERROR] - Domain has no config: '.$http_host);
     }
-
 });
-get('/clearcache', function(){
-    cache_invalidate('domains');
-    echo "Cache flushed";
-    _log(date(DATE_ISO8601).' - [INFO] - Cache flushed');
 
-});
-// serve your site
+// Helper Function to send Syslog Events to our Graylog Server
+function send_remote_syslog($message, $component = "web", $program = "amazee-redirect-custom") {
+  $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+  foreach(explode("\n", $message) as $line) {
+    $syslog_message = "<22>" . date('M d H:i:s ') . $program . ' ' . $component . ': ' . $line;
+    socket_sendto($sock, $syslog_message, strlen($syslog_message), 0, 'amalabs2.nine.ch', 515);
+  }
+  socket_close($sock);
+}
+
 dispatch();
-?>
